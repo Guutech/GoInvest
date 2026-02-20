c4be4d9f7c966d768f13b0b8c717eb457c221530
SeuNome <gbds1815@gmail.com>
2026-02-20 20:34:34 -0300

    Subindo para repositorio remoto

---
+++ src/main/java/GoInvest/GoInvest/GoInvestApplication.java
@@ -0,0 +1,13 @@
+package GoInvest.GoInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GoInvest/GoInvest/GoInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package GoInvest.GoInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
